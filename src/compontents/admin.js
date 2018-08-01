6879f00888f418aa2ff4514efb8073d7bc465cfa
kevin <kevin.e@qq.com>
2018-08-01 20:02:42 +0800

    target shezhi

--- src/compontents/admin.js
+++ src/compontents/admin.js
@@ -12,8 +12,23 @@ function(e) {
       option.dataType = option.data.Type || "json";
       option.crossDomain = true;
       option.xhrFields = {withCredentials: true};
-    $.ajax(option);
-  }
+      $.ajax(option);
+    },
+    getNowDay:function(){
+      var time = new Date();
+      var year = time.getFullYear()
+      ,month = time.getMonth() + 1
+      ,day = time.getDate();
+      if(month < 10)month = '0'+month;
+      if(day < 10)day = '0'+day;
+      return year+'-'+month+'-'+day;
+    },
+    getYearEndDay:function(){
+      var time = new Date();
+      var year = time.getFullYear();
+      return year+'-12-31';
+    }
+
 }
   e("admin", F)
 });
